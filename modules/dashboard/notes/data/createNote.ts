99fc2411c5b209742deb7865d48639a005028e9a
Ferhat Mohamed Tahar <ferhattahar46@gmail.com>
2025-09-03 01:43:52 +0100

    attempt to fix supabase

--- modules/dashboard/notes/data/createNote.ts
+++ modules/dashboard/notes/data/createNote.ts
@@ -14,12 +14,19 @@ export async function createNote({
   if (!user.data.user) {
     return { success: false, error: "User not found" };
   }
+  console.log("Attempting to create note...");
+  console.log("User:", user);
+  console.log("Supabase client initialized:", !!supabase);
+
+  console.log("env", process.env.NEXT_PUBLIC_SUPABASE_URL!);
+
   const { data, error } = await supabase
     .from("notes")
     .insert({ title, content, user_id: user.data.user.id })
     .select()
     .single();
   if (error) {
+    console.error("Database error:", error);
     return { success: false, error };
   }
   return { success: true, data };
